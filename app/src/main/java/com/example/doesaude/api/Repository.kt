58c29abf91d4cebb7345504eda668db6ae20b047
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-10-05 06:09:17 -0300

    Feat: saveInData

--- app/src/main/java/com/example/doesaude/api/Repository.kt
+++ app/src/main/java/com/example/doesaude/api/Repository.kt
@@ -1,6 +1,7 @@
 package com.example.doesaude.api
 
 import com.example.doesaude.model.Categoria
+import com.example.doesaude.model.Postagem
 import retrofit2.Response
 
 class Repository {
@@ -8,4 +9,10 @@ class Repository {
     suspend fun listCategoria(): Response<List<Categoria>>{
         return RetrofitInstance.api.listCategoria()
     }
+
+    suspend fun addPostagem(postagem: Postagem): Response<Postagem>{
+        return RetrofitInstance.api.addPostagem(postagem)
+    }
+
+
 }
--- app/src/main/java/com/example/doesaude/model/Categoria.kt
+++ app/src/main/java/com/example/doesaude/model/Categoria.kt
@@ -1,12 +1,12 @@
 package com.example.doesaude.model
 
 data class Categoria(
-
     var id: Long,
-    var tipo: String?,
     var descricao: String?,
+    var tipo: String?,
+
     var postagens: List<Postagem>?
-    ){
+){
     override fun toString(): String {
         return tipo!!
     }
